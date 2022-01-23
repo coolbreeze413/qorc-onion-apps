852754804cd4a50c8e3277dbe83131704e1d4179
coolbreeze413 <coolbreeze413@yahoo.co.in>
2022-01-24 01:18:05 +0530

    suppress errors from rm while cleaning

--- qorc_fpga_breathectrl/.scaffolding/clean_fpga.sh
+++ qorc_fpga_breathectrl/.scaffolding/clean_fpga.sh
@@ -91,9 +91,9 @@ fi
 PROJECT_ROOT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 PROJECT_RTL_DIR="${PROJECT_ROOT_DIR}/fpga/rtl"
 
-rm -rf "$PROJECT_RTL_DIR"/build || true
-rm "$PROJECT_RTL_DIR"/*bit.h || true
-rm "$PROJECT_RTL_DIR"/*.bin || true
-rm "$PROJECT_RTL_DIR"/*.jlink || true
-rm "$PROJECT_RTL_DIR"/*.openocd || true
-rm "$PROJECT_RTL_DIR"/Makefile.symbiflow || true
+rm -rf "$PROJECT_RTL_DIR"/build 2> /dev/null || true
+rm "$PROJECT_RTL_DIR"/*bit.h 2> /dev/null || true
+rm "$PROJECT_RTL_DIR"/*.bin 2> /dev/null || true
+rm "$PROJECT_RTL_DIR"/*.jlink 2> /dev/null || true
+rm "$PROJECT_RTL_DIR"/*.openocd 2> /dev/null || true
+rm "$PROJECT_RTL_DIR"/Makefile.symbiflow 2> /dev/null || true
